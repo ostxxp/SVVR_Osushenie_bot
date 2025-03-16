4971d92b759d318c5875ec5ff813630411ccf62c
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-03-16 19:57:35 +0300

    Fix objects fetching

--- DB/objects_fetching.py
+++ DB/objects_fetching.py
@@ -2,15 +2,12 @@ from DB.docs_fetching import client
 from DB.prorabs_fetching import prorabs
 
 spreadsheet = client.open_by_key('1OtnMFqU6-m-JsWyFX2GLG6HksRd18K0su4-INlcjN8A')
-worksheet = spreadsheet.sheet1
-all_values = worksheet.get_all_values()
-
-
-headers = all_values[3]
-data = all_values[4:]
-
 
 async def fetch_objects_names(id):
+    worksheet = spreadsheet.sheet1
+    all_values = worksheet.get_all_values()
+    data = all_values[4:]
+
     for prorab in prorabs:
         if prorab[0] != '' and int(prorab[0]) == id:
             name = prorab[1]
@@ -27,12 +24,16 @@ async def fetch_objects_names(id):
     return objects
 
 async def fetch_objects_by_name(name):
+    worksheet = spreadsheet.sheet1
+    all_values = worksheet.get_all_values()
+    data = all_values[4:]
     for d in data:
         if d[1] == name:
             return d
 
 
 def add_link(location, link):
+    worksheet = spreadsheet.sheet1
     worksheet.update(location, [[link]])
 
 
