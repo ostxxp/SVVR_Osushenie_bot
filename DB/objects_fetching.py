3ae6b2204bafdc2ffdd8ed814bdfff64984d3e84
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-03-16 20:03:24 +0300

    Fix prorabs fetching

--- DB/objects_fetching.py
+++ DB/objects_fetching.py
@@ -1,14 +1,14 @@
 from DB.docs_fetching import client
-from DB.prorabs_fetching import prorabs
+from DB.prorabs_fetching import get_prorabs
 
 spreadsheet = client.open_by_key('1OtnMFqU6-m-JsWyFX2GLG6HksRd18K0su4-INlcjN8A')
+worksheet = spreadsheet.sheet1
 
 async def fetch_objects_names(id):
-    worksheet = spreadsheet.sheet1
     all_values = worksheet.get_all_values()
     data = all_values[4:]
 
-    for prorab in prorabs:
+    for prorab in await get_prorabs():
         if prorab[0] != '' and int(prorab[0]) == id:
             name = prorab[1]
             break
@@ -24,7 +24,6 @@ async def fetch_objects_names(id):
     return objects
 
 async def fetch_objects_by_name(name):
-    worksheet = spreadsheet.sheet1
     all_values = worksheet.get_all_values()
     data = all_values[4:]
     for d in data:
@@ -33,7 +32,6 @@ async def fetch_objects_by_name(name):
 
 
 def add_link(location, link):
-    worksheet = spreadsheet.sheet1
     worksheet.update(location, [[link]])
 
 
--- DB/prorabs_fetching.py
+++ DB/prorabs_fetching.py
@@ -4,16 +4,16 @@ spreadsheet = client.open_by_key('1ueuVTglC9wVn4boUiz0S6FjNqWJbmlV43fhh4U4gJAE')
 worksheet = spreadsheet.sheet1
 all_values = worksheet.get_all_values()
 
-headers = all_values[0]
-prorabs = all_values[1:]
+async def get_prorabs():
+    return worksheet.get_all_values()[1:]
 
 async def is_prorab(id):
-    for prorab in prorabs:
+    for prorab in worksheet.get_all_values()[1:]:
         if prorab[0] != '' and int(prorab[0]) == id:
             return True
     return False
 
 async def get_prorab_name(id):
-    for prorab in prorabs:
+    for prorab in worksheet.get_all_values()[1:]:
         if prorab[0] != '' and int(prorab[0]) == id:
             return prorab[1]
