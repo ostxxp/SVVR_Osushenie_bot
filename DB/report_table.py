75072d6f4aee42a753881519d7a3a45764f73a5c
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-03-31 15:14:40 +0300

    Fix type bug

--- DB/report_table.py
+++ DB/report_table.py
@@ -13,18 +13,18 @@ async def create_table_report(id):
     all_values = worksheet.get_all_values()
 
     column = await database_funcs.get_column(id)
-    worksheet.update_cell(2, column, lines[0])
+    worksheet.update_cell(2, int(column), lines[0])
     if len(lines) > 1:
         for k in range(1, len(lines) - 1):
             for i in range(len(all_values)):
                 if str(lines[k].split()[0]) == (all_values[i][0]):
-                    worksheet.update_cell(i + 1, column, float(lines[k].split()[1].strip()))
+                    worksheet.update_cell(i + 1, int(column), float(lines[k].split()[1].strip()))
                     break
         for i in range(len(all_values)):
             if all_values[i][5] == "Рабочие":
                 k = i + 1
                 for installer in sorted(lines[-1].split(',')):
-                    worksheet.update_cell(k, column, installer)
+                    worksheet.update_cell(k, int(column), installer)
                     k += 1
 
 
@@ -40,7 +40,7 @@ async def fill_zeros(id):
 
     with open(f'report_info/{id}.txt', 'r', encoding='utf-8') as file:
         date = file.readlines()
-    worksheet.update_cell(2, column, date[0].strip())
+    worksheet.update_cell(2, int(column), date[0].strip())
 
 
 async def find_date(id, link, date):
