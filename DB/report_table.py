f7df892f57cd7b9fab94c8b5663cdc96d18a9cc6
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-03-31 15:11:27 +0300

    Add Z+ columns support

--- DB/report_table.py
+++ DB/report_table.py
@@ -13,18 +13,18 @@ async def create_table_report(id):
     all_values = worksheet.get_all_values()
 
     column = await database_funcs.get_column(id)
-    worksheet.update(f"{column}2", [[lines[0]]])
+    worksheet.update_cell(2, column, lines[0])
     if len(lines) > 1:
         for k in range(1, len(lines) - 1):
             for i in range(len(all_values)):
                 if str(lines[k].split()[0]) == (all_values[i][0]):
-                    worksheet.update(f"{column}{i + 1}", [[float(lines[k].split()[1].strip())]])
+                    worksheet.update_cell(i + 1, column, float(lines[k].split()[1].strip()))
                     break
         for i in range(len(all_values)):
             if all_values[i][5] == "Рабочие":
                 k = i + 1
                 for installer in sorted(lines[-1].split(',')):
-                    worksheet.update(f"{column}{k}", [[installer]])
+                    worksheet.update_cell(k, column, installer)
                     k += 1
 
 
@@ -40,23 +40,12 @@ async def fill_zeros(id):
 
     with open(f'report_info/{id}.txt', 'r', encoding='utf-8') as file:
         date = file.readlines()
-    worksheet.update(f"{column}2", [[date[0].strip()]])
-
-    for i in range(2, len(all_values)):
-        if all_values[i][5] != "Рабочие" and all_values[i][0].count(".") > 0 and all_values[i][2] != '':
-            worksheet.update(f"{column}{i + 1}", [['0']])
-
+    worksheet.update_cell(2, column, date[0].strip())
 
 
 async def find_date(id, link, date):
     spreadsheet = client.open_by_key(link.split('/')[5])
     worksheet = spreadsheet.sheet1
     values = worksheet.row_values(2)
-
-    for i in range(6, len(values)):
-        if values[i] == date or values[i] == '':
-            await database_funcs.set_column(id, chr(ord("A") + i + 1))
-            return None
-
-    await database_funcs.set_column(id, chr(ord("A") + len(values)))
+    await database_funcs.set_column(id, str(len(values) + 1))
     return None
