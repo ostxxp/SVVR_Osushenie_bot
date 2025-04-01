5e9b38f192cc104d89819790e90bae904f2ea450
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-04-01 09:50:12 +0300

    Debugging

--- bot/start.py
+++ bot/start.py
@@ -17,6 +17,7 @@ router = Router()
 async def start(message: Message, state: FSMContext):
     await database_funcs.clear_reports(message.from_user.id)
     keyboard = await keyboards.objects_to_keyboard(message.from_user.id)
+    print(keyboard)
     if keyboard is not None:
         await message.answer("Здравствуй! Выбери объект:", reply_markup=keyboard)
         if not await database_funcs.prorab_exists(message.from_user.id):
