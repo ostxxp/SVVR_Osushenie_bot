fc2401264bba1ad9819f3e7c6e75cf4981d6cc0f
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-09-01 23:42:35 +0300

    Fix installers fetching

--- DB/installers_fetching.py
+++ DB/installers_fetching.py
@@ -5,5 +5,9 @@ spreadsheet = client.open_by_key("1EPmDWO9po2BOW6ohurI13ojTYocmfKqVmyQotjbyyI0")
 worksheet = spreadsheet.sheet1
 
 async def fetch_installers():
-    all_values = worksheet.get_all_values()
-    return sorted(all_values[1:])
+    all_values = worksheet.get_all_values()[1:]
+    installers = []
+    for i in all_values:
+        if i[1] != '':
+            installers.append(i)
+    return sorted(installers)
